46a091a719b162538b84c826edd63cad8a582c21
RathnaBMohan <rathnamohan19@gmail.com>
2023-08-17 22:46:54 +0530

    Arithmatic Operators
    Arithmatic Operators demo by arithmatic calculator

---
+++ ArithmaticOperatorDemo.java
@@ -0,0 +1,18 @@
+class ArithmaticOperatorDemo
+{
+public static void main(String[] args)
+{
+int a = 10 , b = 20;
+int sum = a + b;
+int difference = a - b;
+int product = a * b;
+int quotient = a/b;
+int remainder = a % b;
+System.out.println("Addition of given numbers = " + sum);
+System.out.println("Subtraction of given numbers = " + difference);
+System.out.println("Multiplication of given numbers = " + product);
+System.out.println("Division of given numbers = " + quotient);
+System.out.println("Modulo Division of given numbers = " + remainder);
+}
+}
+
