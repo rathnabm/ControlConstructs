398650ff74bf4d398ad576a982194710447c778b
RathnaBMohan <rathnamohan19@gmail.com>
2023-08-17 22:59:42 +0530

    Boolean Data Type

---
+++ BooleanDataTypeDemo.java
@@ -0,0 +1,14 @@
+class BooleanDataTypeDemo
+{
+public static void main(String[] args)
+{
+//boolean b= true;
+//boolean b= false;
+//boolean b= True;
+//boolean b= TRUE;
+//boolean b= FALSE;
+//boolean b = 'True';
+boolean b="True";
+System.out.print("The value of b = " +b);
+}
+}
